e09bae8221a23eee89bdcd207007a82e90db1a01
Ankur Gattani <ankurgattani02@gmail.com>
2025-10-30 17:57:18 +0530

    Add Admin Dashboard and Nav Card components with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,34 @@
 import { Routes } from '@angular/router';
 import { Signup } from './pages/signup/signup';
 import { Login } from './pages/login/login';
+import { AdminDashboard } from './pages/admin-dashboard/admin-dashboard';
 
 export const routes: Routes = [
   {
-	path: 'signup',
-	component: Signup,
-	pathMatch: 'full',
+    path: 'signup',
+    component: Signup,
+    pathMatch: 'full',
   },
   {
-    path:'login',
-    component:Login,
-    pathMatch:'full',
+    path: 'login',
+    component: Login,
+    pathMatch: 'full',
   },
-  
+  {
+    path: 'admin/dashboard',
+    component: AdminDashboard
+  },
+  // {
+  //   path: 'admin/exams',
+  //   loadComponent: () => import('./pages/exam-management/exams').then(m => m.Exams)
+  // },
+  // {
+  //   path: 'admin/questionbank',
+  //   loadComponent: () => import('./pages/question-bank/questionbank').then(m => m.QuestionBank)
+  // },
+  // {
+  //   path: 'admin/reports',
+  //   loadComponent: () => import('./pages/reports/reports').then(m => m.Reports)
+  // },
+
 ];
