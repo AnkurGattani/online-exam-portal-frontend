9ee26f6bd36363f2da2498778f267d18b04bf2c9
Chandra Sekhar Challa <chandrasekharchalla.45@gmail.com>
2025-10-30 18:48:41 +0530

    Implement role-based access control and enhance login functionality with JWT authentication

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-// src/main.ts
-
 import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app'; // Or AppComponent
-import { appConfig } from './app/app.config';
+import { App } from './app/app'; // Your root component
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { routes } from './app/app.routes';
+import { Interceptor } from './app/services/interceptor/interceptor';
 
-
-bootstrapApplication(App, appConfig)
-  .catch(err => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([Interceptor])) //  Register interceptor here
+  ]
+}).catch(err => console.error(err));
