7ed6659b4a5b2c6d2ba765183561925adabaa497
Ankur Gattani <95461354+AnkurGattani@users.noreply.github.com>
2025-10-31 13:09:13 +0530

    Uncomment app bootstrap and configuration lines

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { App } from './app/app'; // Your root component
+import { App } from './app/app'; // Root component
+// import { appConfig } from './app/app.config';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { Interceptor } from './app/services/interceptor/interceptor';
+
+// bootstrapApplication(App, appConfig)
+//   .catch(err => console.error(err));
 
 bootstrapApplication(App, {
   providers: [
