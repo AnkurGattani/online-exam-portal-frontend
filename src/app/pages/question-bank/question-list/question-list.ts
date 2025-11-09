6802cec7e35786c56e1d96505d9384ffbde42543
Ankur Gattani <ankurgattani02@gmail.com>
2025-11-09 19:59:59 +0530

    feat: Add filtering and sorting functionality to question list; enhance UI with filter options

--- src/app/pages/question-bank/question-list/question-list.ts
+++ src/app/pages/question-bank/question-list/question-list.ts
@@ -6,18 +6,34 @@ import { MatListModule } from '@angular/material/list';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { MatAnchor } from "@angular/material/button";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-question-list',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatListModule, MatAnchor],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatListModule,
+    MatAnchor,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule
+  ],
   templateUrl: './question-list.html',
   styleUrl: './question-list.css',
 })
 export class QuestionList implements OnInit {
 
   questionList: Question[] = [];  // Initialize an empty array and make sure it's not undefined and of Question type
+  filteredQuestions: Question[] = [];
+
+  filterType: string = 'ALL';
+  filterDifficulty: string = 'ALL';
+  sortOrder: string = 'NONE';
 
   // Constructor with dependency injection
   constructor(private questionService: QuestionService, private router: Router) { }
@@ -29,7 +45,28 @@ export class QuestionList implements OnInit {
       // console.log(this.questionList);
       // console.log(this.questionList[0].options[0].optionText);
 
+      this.filteredQuestions = [...this.questionList];
     });
+  }
+
+  applyFilters(): void {
+    let filteredCriteria = [...this.questionList];
+
+    if (this.filterType !== 'ALL') {
+      filteredCriteria = filteredCriteria.filter(q => q.questionType === this.filterType);
+    }
+
+    if (this.filterDifficulty !== 'ALL') {
+      filteredCriteria = filteredCriteria.filter(q => q.difficulty === this.filterDifficulty);
+    }
+
+    if (this.sortOrder === 'asc') {
+      filteredCriteria.sort((a, b) => a.marks - b.marks);
+    } else if (this.sortOrder === 'desc') {
+      filteredCriteria.sort((a, b) => b.marks - a.marks);
+    }
+
+    this.filteredQuestions = filteredCriteria;
   }
 
   editQuestion(question: Question): void {
